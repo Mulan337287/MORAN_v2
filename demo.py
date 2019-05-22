d43fadd917866a0c3982e668e663a2d625e68dd8
Mulan337287 <48011433+Mulan337287@users.noreply.github.com>
2019-05-22 14:58:04 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,6 +11,7 @@ model_path = './demo.pth'
 img_path = './demo/0.png'
 alphabet = '0:1:2:3:4:5:6:7:8:9:a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s:t:u:v:w:x:y:z:$'
 
+################# 1-load model #######################################
 cuda_flag = False
 if torch.cuda.is_available():
     cuda_flag = True
@@ -33,15 +34,15 @@ MORAN.load_state_dict(MORAN_state_dict_rename)
 for p in MORAN.parameters():
     p.requires_grad = False
 MORAN.eval()
-
-converter = utils.strLabelConverterForAttention(alphabet, ':')
+################# 2-load data #######################################
+converter = utils.strLabelConverterForAttention(alphabet, ':')#得到所有待识别字符的类别编号
 transformer = dataset.resizeNormalize((100, 32))
 image = Image.open(img_path).convert('L')
-image = transformer(image)
+image = transformer(image) #读取灰度图像并将其转换成100*32(w,h), image:1x32x100
 
 if cuda_flag:
     image = image.cuda()
-image = image.view(1, *image.size())
+image = image.view(1, *image.size()) # 1x1x32x100
 image = Variable(image)
 text = torch.LongTensor(1 * 5)
 length = torch.IntTensor(1)
@@ -49,21 +50,23 @@ text = Variable(text)
 length = Variable(length)
 
 max_iter = 20
-t, l = converter.encode('0'*max_iter)
-utils.loadData(text, t)
+t, l = converter.encode('0'*max_iter) # 初始化文本内容和文本长度t=20*'0', l=20
+utils.loadData(text, t) #将初始化的值赋值到text和l上
 utils.loadData(length, l)
-output = MORAN(image, length, text, text, test=True, debug=True)
 
-preds, preds_reverse = output[0]
-demo = output[1]
+################# 3-模型输出 #######################################
+output = MORAN(image, length, text, text, test=True, debug=True) #这里初始双向的结果
+
+preds, preds_reverse = output[0] #双向结果
+demo = output[1] #test debug阶段输出矫正的文本
 
 _, preds = preds.max(1)
 _, preds_reverse = preds_reverse.max(1)
 
-sim_preds = converter.decode(preds.data, length.data)
-sim_preds = sim_preds.strip().split('$')[0]
-sim_preds_reverse = converter.decode(preds_reverse.data, length.data)
-sim_preds_reverse = sim_preds_reverse.strip().split('$')[0]
+sim_preds = converter.decode(preds.data, length.data) #将预测的文本概率转换成文本, jewelers$e$e$e$
+sim_preds = sim_preds.strip().split('$')[0] #jewelers
+sim_preds_reverse = converter.decode(preds_reverse.data, length.data) #srelewej$$$$$seej$$$
+sim_preds_reverse = sim_preds_reverse.strip().split('$')[0]#srelewej
 
 print('\nResult:\n' + 'Left to Right: ' + sim_preds + '\nRight to Left: ' + sim_preds_reverse + '\n\n')
 
